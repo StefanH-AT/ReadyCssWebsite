b5607644ac5e5b76e7f6886f81bf4bb882f08b33
Tewan <tewan@spycesoftware.com>
2022-02-15 14:15:32 +0100

    Update buttons code

--- readycss-website/src/ButtonPage.vue
+++ readycss-website/src/ButtonPage.vue
@@ -7,12 +7,16 @@
       <button class="btn btn-outline">Button</button>
       <button class="btn btn-fg">Button</button>
       <button class="btn btn-fg btn-outline">Button</button>
-      <button class="btn btn-mg">Button</button>
-      <button class="btn btn-mg btn-outline">Button</button>
+      <button class="btn btn-bg">Button</button>
+      <button class="btn btn-bg btn-outline">Button</button>
     </div>
     <div class="group palette-notice">
       <button class="btn">Button</button>
       <button class="btn btn-outline">Button</button>
+      <button class="btn btn-fg">Button</button>
+      <button class="btn btn-fg btn-outline">Button</button>
+      <button class="btn btn-bg">Button</button>
+      <button class="btn btn-bg btn-outline">Button</button>
     </div>
   </main>
 
