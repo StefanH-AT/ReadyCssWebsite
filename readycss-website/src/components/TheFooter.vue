1ef31b8a26e9c5cd03552edd489eda0343396f56
Tewan <tewan@spycesoftware.com>
2022-02-22 09:35:46 +0100

    Add footer

---
+++ readycss-website/src/components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+<footer>
+  <a href="https://github.com/StefanH-AT/ReadyCSS">GitHub</a>
+  <a href="https://github.com/StefanH-AT/ReadyCSSWebsite">GitHub Website</a>
+</footer>
+</template>
+
+<script lang="ts">
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+footer {
+  --footer-spacer: calc(var(--spacer) * 2);
+  display: grid;
+  padding: var(--footer-spacer);
+  margin-top: var(--footer-spacer);
+
+  place-items: center;
+  grid-auto-flow: column;
+
+  background: var(--scheme-shade-7);
+}
+</style>
