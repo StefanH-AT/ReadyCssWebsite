6a61ce5e5b43ce80fbd39971cf6f2156aa90d545
Tewan <tewan@spycesoftware.com>
2022-03-02 21:03:14 +0100

    Work on checkbox page

--- readycss-website/src/CheckPage.vue
+++ readycss-website/src/CheckPage.vue
@@ -6,11 +6,17 @@
     <div class="callout">
       <p>
         <strong>Hint:</strong>
-        It is important the label is assigned the <code>for</code> attribute. This way, the box can be clicked by clicking the label, which can make it easier to point to.
+        It is important the label is assigned the <code>for</code> attribute. This way, the box can be toggled by clicking the label, which can make it easier to point to.
       </p>
     </div>
     <code-sample lang="html" preview :code="exampleSimple"/>
 
+  </div>
+
+  <div class="docs-group">
+    <h3>Palettes</h3>
+    <p>Apply a palette to change the color of the checkbox</p>
+    <code-sample lang="html" preview :code="examplePalette"/>
   </div>
 
 </template>
@@ -22,7 +28,9 @@ export default {
   data() {
     return {
       exampleSimple: `<input class="input-check" type="checkbox" id="check1">
-<label for="check1">Check me out</label>`
+<label for="check1">Check me out</label>`,
+      examplePalette: `<input class="input-check palette-notice" type="checkbox" id="check2">
+<label for="check2">Check me out</label>`
     }
   }
 }
