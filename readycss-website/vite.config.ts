d434ee78f9024c77b41702fd8553e3909acae96f
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-01-20 23:07:09 +0100

    Add @ import absolute path alias for ts imports

--- readycss-website/vite.config.ts
+++ readycss-website/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), tsconfigPaths()],
     resolve: {
         alias: {
-            "vue": "vue/dist/vue.esm-bundler.js"
+            "vue": "vue/dist/vue.esm-bundler.js",
+            "@": "/src",
         }
     }
 });
