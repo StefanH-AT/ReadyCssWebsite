00739f3578da41e1ef5a47bce261f77d7fdc26f2
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-01-20 23:05:31 +0100

    Separate router into separate file

--- readycss-website/src/main.ts
+++ readycss-website/src/main.ts
@@ -1,23 +1,9 @@
 import { createApp } from "vue";
-import {createRouter, createWebHistory, NavigationFailure, RouteLocationNormalized, RouteRecordRaw} from "vue-router";
 import themePlugin from "./plugins/theme-plugin";
 
 import "./scss/main.scss";
 
-const routes: RouteRecordRaw[] = [
-    { path: "/", component: () => import("./HomePage.vue"), name: "Color management library" },
-    { path: "/button", component: () => import("./ButtonPage.vue"), name: "Buttons" },
-    { path: "/textinputs", component: () => import("./InputTextPage.vue"), name: "Text Inputs" },
-    { path: "/check", component: () => import("./CheckPage.vue"), name: "Checkboxes" }
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-router.afterEach((to: RouteLocationNormalized, from: RouteLocationNormalized, failure: NavigationFailure | void) => {
-    document.querySelector("title").innerText = `ReadyCSS - ${to.name.toString()}`;
-});
+import { router } from "./router";
 
 import TheNavbar from "./components/TheNavbar.vue";
 import TheFooter from "./components/TheFooter.vue";
---
+++ readycss-website/src/router.ts
@@ -0,0 +1,18 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router"
+
+const routes: RouteRecordRaw[] = [
+    { path: "/", component: () => import("./pages/HomePage.vue"), name: "Color management library" },
+    { path: "/button", component: () => import("./pages/ButtonPage.vue"), name: "Buttons" },
+    { path: "/textinputs", component: () => import("./pages/InputTextPage.vue"), name: "Text Inputs" },
+    { path: "/check", component: () => import("./pages/CheckPage.vue"), name: "Checkboxes" },
+    { path: "/gallery", component: () => import("./pages/GalleryPage.vue"), name: "Gallery" },
+    { path: "/gallery/palette", component: () => import("./pages/gallery/PalettePage.vue"), name: "Palettes" },
+    { path: "/gallery/reel", component: () => import("./pages/gallery/ReelPage.vue"), name: "Reel" },
+    { path: "/gallery/switcher", component: () => import("./pages/gallery/SwitcherPage.vue"), name: "Switcher" },
+    { path: "/gallery/sidebar", component: () => import("./pages/gallery/SidebarPage.vue"), name: "Sidebar" },
+];
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
