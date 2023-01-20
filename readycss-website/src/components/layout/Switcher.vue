19c84434604153d69d300e5b9acfadff6b8ab745
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-01-20 23:02:16 +0100

    Add layout components

---
+++ readycss-website/src/components/layout/Switcher.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="switcher">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps<{
+  limit: {
+    required: false,
+    type: Number,
+    default: 10,
+  },
+  space: {
+    required: false,
+    type: String,
+    default: "",
+  },
+  threshold: {
+    required: true,
+    type: Number,
+  },
+}>()
+</script>
+
+<style>
+
+.switcher {
+  display: flex;
+  flex-wrap: wrap;
+  gap: v-bind("props.space");
+}
+
+.switcher > * {
+  flex-grow: 1;
+  flex-basis: calc((v-bind("props.threshold") - 100%) * 999);
+}
+
+</style>
