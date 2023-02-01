45d1db53b4a789ed34b956443e249e16e8687582
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-02-01 17:50:44 +0100

    Update navbar

--- readycss-website/src/components/TheNavbar.vue
+++ readycss-website/src/components/TheNavbar.vue
@@ -8,38 +8,32 @@
           <li><router-link to="/gallery">Gallery</router-link></li>
           <li><a href="https://github.com/StefanH-AT/ReadyCss"><i class="fab fa-github"></i> GitHub</a></li>
         </ul>
-        <button @click="toggleTheme" class="switch-theme">{{ themeName }}</button>
+        <button @click="themePlugin.toggleTheme()" class="switch-theme">{{ themePlugin.getThemeName() }}</button>
       </nav>
     </div>
 
   </header>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import * as themePlugin from "@/plugins/theme-plugin";
 
-export default {
-  methods: {
-    toggleTheme() {
-      themePlugin.toggleTheme();
-    }
-  },
-  computed: {
-    isDark: () => themePlugin.isDark(),
-    themeName: () => themePlugin.getThemeName()
-  }
-}
 </script>
 
 <style scoped lang="scss">
 
-.nav {
+header {
   background-color: var(--scheme-surface-0);
   box-shadow: 0 0.1rem 0.75rem 0 var(--scheme-shadow);
   padding-block: calc(var(--spacer) * 0.75);
+}
+
+.nav {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+  max-width: var(--page-width-reader);
+  margin-inline: auto;
 
   h1 {
     font-size: 1.6rem;
